08f3604f637a8b3c8ee77c44460622160c24e4c5
DennisAronJensen <69842874+DennisAronJensen@users.noreply.github.com>
2021-02-12 13:36:08 +0100

    Create question-1.js

---
+++ question-1.js
@@ -0,0 +1,19 @@
+/*
+Module Assignment
+
+Level 1
+
+Question 1
+
+Convert the function below to an arrow function:
+
+function division(a, b) {    
+    return a % b;
+}
+*/
+
+division = (a, b) => a / b;
+
+console.log(division(5, 9));
+
+
